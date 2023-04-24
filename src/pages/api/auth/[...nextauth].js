9e5d03fe2863bf1d4ce4199b96344c97fdf8b4be
shawn2776 <shawn.harrington2776@gmail.com>
2023-04-24 08:06:36 -0700

    finished db connection keeping sec info

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../../lib/mongodb";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,5 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });
