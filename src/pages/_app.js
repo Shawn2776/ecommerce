1c6d5e8b8824721b38cdbb2448df28f7061e9183
shawn2776 <shawn.harrington2776@gmail.com>
2023-04-24 07:50:37 -0700

    basic auth added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import { Oswald } from "next/font/google";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const oswald = Oswald({ subsets: ["latin"] });
+
+export default function App({ session, Component, pageProps }) {
+  return (
+    <SessionProvider session={session}>
+      <div className={oswald.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
 }
